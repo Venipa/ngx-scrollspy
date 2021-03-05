20b3b5dddf15ce9d3645d8647f24d8f4c17f9772
Venipa <admin@venipa.net>
2021-03-05 16:55:56 +0100

    exported required components

--- projects/ngx-scrollspy/src/public-api.ts
+++ projects/ngx-scrollspy/src/public-api.ts
@@ -6,6 +6,9 @@ export * from './lib/ngx-scrollspy.service';
 export * from './lib/ngx-scrollspy.component';
 export * from './lib/ngx-scrollspy.module';
 
+export * from './lib/scrollSpyAffix/scrollSpyAffix.directive'
 export * from './lib/scrollSpyAffix/scrollSpyAffix.module'
+export * from './lib/scrollSpyInfinite/scrollSpyInfinite.directive'
 export * from './lib/scrollSpyInfinite/scrollSpyInfinite.module'
+export * from './lib/scrollSpyParallax/scrollSpyParallax.directive'
 export * from './lib/scrollSpyParallax/scrollSpyParallax.module'
